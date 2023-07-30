f3442b7a64ba1593340bd03d34d43effd4c977ab
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-30 17:58:54 +0200

    Added dark mode toggler

---
+++ src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import Icon from "@iconify/svelte";
+
+    let isDarkMode = false;
+
+    onMount(() => {
+        if (localStorage.theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    })
+</script>
+
+<button
+class="flex gap-3 items-center text-[1.6rem] font-semibold tracking-wide"
+ on:click={() => {
+    isDarkMode = document.documentElement.classList.toggle("dark");
+    if (isDarkMode) localStorage.setItem("theme", "dark");
+    else localStorage.setItem("theme", "light");
+    }}>
+    {#if !isDarkMode}
+    <Icon icon="ph:moon" />
+    <span>
+        Dark Mode</span>
+    {:else} 
+        <Icon icon="ph:sun"/>
+    <span>Light Mode</span>
+    {/if}
+
+</button>
