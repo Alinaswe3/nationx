faba3209b4765a3917765656e49380d8969f01a9
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-30 15:33:35 +0200

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.postcss";
+  import "@fontsource-variable/nunito-sans";
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    font-family: "Nunito Sans", sans-serif;
+  }
+</style>
