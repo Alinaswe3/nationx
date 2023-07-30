98a2fda9a47851b11cbb04d71b92ecec515830c8
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-30 16:49:04 +0200

    Added style colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,15 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "darker-blue": "hsl(207, 26%, 17%)",
+        "very-dark-blue": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "light-gray": "hsl(0, 0%, 98%)",
+      },
+    },
   },
 
   plugins: [require("daisyui")],
